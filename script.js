51a332feeed00a772d089d2f799ad963e4ef97cc
sahilpatel0oo7 <109283616+sahilpatel0oo7@users.noreply.github.com>
2023-03-30 09:45:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,129 @@
+flash = 1
+
+function b1() {
+    
+    if (flash == 1) { 
+        document.getElementById("b1").value = "X";
+        document.getElementById("b1").disabled = true;
+        flash = 0
+
+
+    }
+
+    else {document.getElementById("b1").value = "0";
+    document.getElementById("b1").disabled = true;
+    flash = 1}
+
+}
+function b2() {
+    
+    if (flash == 1) { 
+        document.getElementById("b2").value = "X"
+        document.getElementById("b2").disabled = true;
+     flash = 0}
+
+    else {document.getElementById("b2").value = "0";
+    document.getElementById("b2").disabled = true;
+    flash = 1}
+
+}
+function b3() {
+    
+    if (flash == 1) { 
+        document.getElementById("b3").value = "X"
+        document.getElementById("b3").disabled = true;
+     flash = 0}
+
+    else {document.getElementById("b3").value = "0";
+    document.getElementById("b3").disabled = true;
+    flash = 1}
+
+}
+function b4() {
+    
+    if (flash == 1) { 
+        document.getElementById("b4").value = "X"
+        document.getElementById("b4").disabled = true;
+     flash = 0}
+
+    else {document.getElementById("b4").value = "0";
+     document.getElementById("b4").disabled = true;
+    flash = 1}
+
+}
+function b5() {
+    
+    if (flash == 1) { 
+        document.getElementById("b5").value = "X"
+        document.getElementById("b5").disabled = true;
+     flash = 0}
+
+    else {document.getElementById("b5").value = "0";
+    document.getElementById("b5").disabled = true;
+    flash = 1}
+
+}
+function b6() {
+    
+    if (flash == 1) { 
+        document.getElementById("b6").value = "X"
+        document.getElementById("b6").disabled = true;
+     flash = 0}
+
+    else {document.getElementById("b6").value = "0";
+    document.getElementById("b6").disabled = true;
+    flash = 1}
+
+}
+function b7() {
+    
+    if (flash == 1) { 
+        document.getElementById("b7").value = "X"
+        document.getElementById("b7").disabled = true;
+     flash = 0}
+
+    else {document.getElementById("b7").value = "0";
+    document.getElementById("b7").disabled = true;
+    flash = 1}
+
+}
+function b8() {
+    
+    if (flash == 1) { 
+        document.getElementById("b8").value = "X"
+        document.getElementById("b8").disabled = true;
+     flash = 0}
+
+    else {document.getElementById("b8").value = "0";
+    document.getElementById("b8").disabled = true;
+    flash = 1}
+
+}
+function b9() {
+    
+    if (flash == 1) { 
+        document.getElementById("b9").value = "X"
+        document.getElementById("b9").disabled = true;
+     flash = 0}
+
+    else {document.getElementById("b9").value = "0";
+    document.getElementById("b9").disabled = true;
+    flash = 1}
+
+}
+
+function refresh(){
+    location.reload();
+    document.getElementById("b1").value = ""; 
+    document.getElementById("b2").value = ""; 
+    document.getElementById("b3").value = ""; 
+    document.getElementById("b4").value = ""; 
+    document.getElementById("b5").value = ""; 
+    document.getElementById("b6").value = ""; 
+    document.getElementById("b7").value = ""; 
+    document.getElementById("b8").value = ""; 
+    document.getElementById("b9").value = ""; 
+    
+   
+}
+
